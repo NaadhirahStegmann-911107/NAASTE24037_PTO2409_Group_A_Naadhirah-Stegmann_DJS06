0c36097ba2ad84f9dbab614c0f3ef31fab7d224c
Naadhirah Stegmann <naadhirahsteg@gmail.com>
2025-05-30 21:37:38 +0200

    ForEach Basics
    Used the forEach to iterate over the arrays to display details then used the index to combine inputs into log the results in the format required.

--- scripts.js
+++ scripts.js
@@ -13,3 +13,13 @@ const products = [
   { product: 'coffee', price: 10 },
   { product: 'tea', price: '' },
 ]
+
+// 1. ForEach Basics
+// Log each name
+names.forEach(name => console.log(name));
+// Log each province
+provinces.forEach(province => console.log(province));
+//Log name with matching province
+names.forEach((name, index) => {
+  console.log(`${name} (${provinces[index]})`)
+});
